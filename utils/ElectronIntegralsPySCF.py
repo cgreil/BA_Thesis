3a639ffdf32662e4fbb12730a567d35f9b768586
Christoph <c.moser400@gmail.com>
2023-01-19 09:12:51 +0100

    Added custom basis file

--- utils/ElectronIntegralsPySCF.py
+++ utils/ElectronIntegralsPySCF.py
@@ -1,6 +1,7 @@
 import pyscf
 from pyscf import gto
 from pyscf.gto.basis import parse_gaussian
+import numpy as np
 
 
 def create_beh2():
@@ -15,8 +16,8 @@ def create_beh2():
     mol.unit='B' #B for Bohr
     mol.basis={
         #For WSL interpreter use linux like paths
-        'Be': parse_gaussian.load('/mnt/c/Users/cmose/PycharmProjects/BA/Calculation/resources/BeH2_basis.gbs', 'Be'),
-        'H': parse_gaussian.load('/mnt/c/Users/cmose/PycharmProjects/BA/Calculation/resources/BeH2_basis.gbs', 'H')
+        'Be': parse_gaussian.load('/mnt/c/Users/cmose/PycharmProjects/BA/Calculation/resources/BeH2_basis_new.gbs', 'Be'),
+        'H': parse_gaussian.load('/mnt/c/Users/cmose/PycharmProjects/BA/Calculation/resources/BeH2_basis_new.gbs', 'H')
         #'Be': parse_gaussian.load('C:\\Users\\cmose\\PycharmProjects\\BA\\Calculation\\resources\\BeH2_basis.gbs', 'Be'),
         #'H': parse_gaussian.load('/C:\\Users\\cmose\\PycharmProjects\\BA\\Calculation\\resources\\BeH2_basis.gbs', 'H')
 
@@ -26,7 +27,6 @@ def create_beh2():
     mol.build()
     #mol.symmetry = True
     #mol.symmetry_subgroup = 'DooH'
-
     return mol
 
 
@@ -51,7 +51,7 @@ def get_1e_integrals(molecule):
 def get_2e_integrals(molecule):
     """Function to calculate the 2-electron integrals in PySCF and returning the respective values as NxN matrix
     for a given molecule"""
-    h2e = h2e = molecule.intor("int2e", aosym='s1')
+    h2e = molecule.intor("int2e", aosym='s1')
     return h2e
 
 
@@ -60,10 +60,17 @@ if __name__ == '__main__':
     beh2 = create_beh2()
 
     #print reference
-    print_beh2_HF_energy()
+    #print_beh2_HF_energy()
     print("1-Electron interaction integrals (kinetic + nuclear)")
-    print(len(get_1e_integrals(beh2)))
+    eri1 = np.array(get_1e_integrals(beh2))
+    print(eri1.shape)
     print("2-Electron interaction integrals:")
-    print(len(get_2e_integrals(beh2)))
+    eri2 = np.array(get_2e_integrals(beh2))
+    print(eri2.shape)
+
+    #reshape eri2 so it can be written to file
+    N = len(eri2)
+    np.savetxt('eri1', eri1, fmt='%4.6f', delimiter=' ')
+    np.savetxt('eri2', eri2.reshape((N*N,N*N)), fmt='%4.6f', delimiter=' ')
 
 
