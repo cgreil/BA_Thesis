adc402b52b66d142af2a31ec7a286e38bd2b3a52
Christoph <c.moser400@gmail.com>
2023-02-23 15:48:15 +0100

    Added LinearPauliStringCombination

--- src/decomposition/PauliString.py
+++ src/decomposition/PauliString.py
@@ -11,9 +11,9 @@ class PauliString:
     pauli_tuple: tuple
 
     # Pauli List is a tuple containing either Pauli objects or LinearPauliCombination objects
-    def __init__(self, pauli_list: tuple[Pauli | LinearPauliCombination], weight):
+    def __init__(self, pauli_tuple: tuple[Pauli | LinearPauliCombination], weight):
         self.weight = weight
-        self.pauli_list = pauli_list
+        self.pauli_tuple = pauli_tuple
 
     def __len__(self):
         return len(self.pauli_tuple)
@@ -24,6 +24,16 @@ class LinearPauliCombination:
     pauli_op_1: Pauli
     pauli_op_2: Pauli
 
-    def __int__(self, pauli1: Pauli, pauli2: Pauli):
+    def __init__(self, pauli1: Pauli, pauli2: Pauli):
         self.pauli_op_1 = pauli1
         self.pauli_op_2 = pauli2
+
+
+class LinearPauliStringCombination:
+    """Datastructure to allow a linear combination of two full Pauli Strings"""
+    pauli_string_1: PauliString
+    pauli_string_2: PauliString
+
+    def __init__(self, pauli_string_1, pauli_string_2):
+        self.pauli_string_1 = pauli_string_1
+        self.pauli_string_2 = pauli_string_2
