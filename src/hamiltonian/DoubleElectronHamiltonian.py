b608cad81b97334aa010ba5fedb4073797b41bc7
christoph <c.moser400@gmail.com>
2023-04-08 14:21:40 +0200

    added diagonal pauli generation function

--- src/hamiltonian/DoubleElectronHamiltonian.py
+++ src/hamiltonian/DoubleElectronHamiltonian.py
@@ -34,15 +34,27 @@ def _generate_diagonal_paulis(num_qubits: int, weights: NDArray[Shape['4'], Floa
             coeffs[coeff_index] = (1 / 4) * weights[i, j, i, j]
             coeff_index = coeff_index + 1
 
+            pauli_identity_string = _identity_string_builder(num_qubits)
+            pauli_Zi_string = _pauli_single_Z_string_builder(num_qubits, i)
+            pauli_Zj_string = _pauli_single_Z_string_builder(num_qubits, j)
+            pauli_double_Z_string = _pauli_double_Z_string_builder(num_qubits, i, j)
+
+            # append all strings to the sum
+            pauli_list.extend([pauli_identity_string, pauli_Zi_string, pauli_Zj_string, pauli_double_Z_string])
+
+    # finally create the SparsePauliOp
+    return SparsePauliOp(pauli_list, coeffs=np.array(coeffs))
+
 
 def _identity_string_builder(num_qubits: int):
     # pass none to get identity string back
     return pauli_string_from_dict(num_qubits, None)
 
 
-def _pauli_single_z_string_builder(num_qubits: int, i: int):
+def _pauli_single_Z_string_builder(num_qubits: int, i: int):
     return pauli_string_from_dict(num_qubits, {i: 'Z'})
 
 
-def _pauli_double_z_string_builder(num_qubits: int, i: int, j: int):
+def _pauli_double_Z_string_builder(num_qubits: int, i: int, j: int):
     return pauli_string_from_dict(num_qubits, {i: 'Z', j: 'Z'})
+
