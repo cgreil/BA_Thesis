4e17dad9903e8bae694de86a058afb756996ec47
christoph <c.moser400@gmail.com>
2023-04-11 13:50:48 +0200

    Changed MEthods to classmethods

--- src/resources/DoubleElectronInteractionData.py
+++ src/resources/DoubleElectronInteractionData.py
@@ -11,15 +11,18 @@ class DoubleElectronInteractionData:
     _pauli_list = ['XXXX', 'XXYY', 'XYXY', 'XYYX', 'YXXY', 'YXYX', 'YYXX', 'YYYY']
     _sign_lookup_matrix = [[1, 1, 1], [-1, 1, 1], [1, -1, 1], [1, 1, -1], [1, 1, -1], [1, -1, 1], [-1, 1, 1], [1, 1, 1]]
 
-    def get_number_of_terms(self):
-        return self._number_of_terms
+    @classmethod
+    def get_number_of_terms(cls):
+        return cls._number_of_terms
 
-    def get_pauli_list(self):
-        return self._pauli_list
+    @classmethod
+    def get_pauli_list(cls):
+        return DoubleElectronInteractionData._pauli_list
 
-    def get_sign(self, interaction_group: InteractionGroup, term_number: int):
+    @classmethod
+    def get_sign(cls, interaction_group: InteractionGroup, term_number: int):
         """Returns the sign of a term that is specified by the interaction group and the
         term number. Specifically, the sign is in the lookup matrix at entry i,j where row i
         corresponds to the term number and interaction group j corresponds to the column"""
         # Notice that the enum interaction group has integer correspondence
-        return self._sign_lookup_matrix[term_number][interaction_group.value]
+        return cls._sign_lookup_matrix[term_number][interaction_group.value]
