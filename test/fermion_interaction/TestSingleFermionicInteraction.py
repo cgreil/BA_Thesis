cd47a47ad6f51e95010b47600ed3c0e540eecfe8
christoph <c.moser400@gmail.com>
2023-04-12 20:59:16 +0200

    Added unittests for x_string_builder

--- test/fermion_interaction/TestSingleFermionicInteraction.py
+++ test/fermion_interaction/TestSingleFermionicInteraction.py
@@ -2,6 +2,7 @@ import unittest
 
 from src.fermion_interaction.SingleFermionicInteraction import _pauli_X_string_builder as build
 
+
 class TestSingleFermionicInteraction(unittest.TestCase):
 
     def test_pauli_x_string_builder_odd_qubits(self):
@@ -17,6 +18,16 @@ class TestSingleFermionicInteraction(unittest.TestCase):
     def test_pauli_x_string_builder_x_at_bounds(self):
         correctstring = 'XZZZX'
         built_string = build(5, 0, 4)
+        self.assertEquals(built_string, correctstring)
+
+    def test_pauli_x_string_builder_adjacent_x(self):
+        correctstring = 'IIXXII'
+        built_string = build(6, 2, 3)
+        self.assertEquals(built_string, correctstring)
+
+    def test_pauli_x_string_builder_only_x(self):
+        correctstring = 'XX'
+        built_string = build(2, 0, 1)
         self.assertEquals(built_string, correctstring)
 
     def test_pauli_x_string_builder_equal_indices(self):
