0a0fee36914733950c167e9bf34c9093ef858ad3
christoph <c.moser400@gmail.com>
2023-04-12 18:35:20 +0200

    Added RandomWeightGenerator.py

---
+++ src/ansatz/RandomWeightGenerator.py
@@ -0,0 +1,19 @@
+"""Creates the random weights needed for the k-UCC Ansatz state"""
+
+from nptyping import NDArray, Shape, Float
+
+from random import random
+import numpy as np
+
+def generate_random_2dim(num_qubits: int) -> NDArray[Shape['2'], Float]:
+    """Generates a random numpy ndarray with dimension num_qubits x num_qubits as initial values
+    for the k-UCC Ansatz state
+    """
+    return np.random.rand(num_qubits, num_qubits)
+
+
+def generate_random_4dim(num_qubits: int) -> NDArray[Shape['4'], Float]:
+    """Generates a random numpy ndarray with dimension num_qubits x num_qubits x num_qubits x num_qbits
+     as initial values for the k-UCC Ansatz state
+    """
+    return np.random.rand(num_qubits, num_qubits, num_qubits, num_qubits)
