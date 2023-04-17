0ba6b6284a24f173f46e10cac665672dd26ad3be
christoph <c.moser400@gmail.com>
2023-04-17 11:13:17 +0200

    Added full_op and expectation_value implementation

--- src/VQE/VQEBuilder.py
+++ src/VQE/VQEBuilder.py
@@ -17,17 +17,17 @@ class VQEBuilder:
     """Class which includes static methods that provide the circuit creation functionality for the VQE"""
 
     @staticmethod
-    def build_hamiltonian_operator(self, num_qubits):
+    def build_hamiltonian_operator(num_qubits):
         """Creates the hamiltonian class for num_qubits, stores the operator"""
         hamiltonian = FermionicHamiltonian(num_qubits)
         hamiltonian_operator = hamiltonian.get_hamiltonian()
 
-        return hamiltonian_operator.exp_i()
+        return hamiltonian_operator
 
     @staticmethod
     def build_reference_state_operator(num_qubits: int, num_occupied: int):
         """For a VQE with num_qubits qubits (corresponding to the number of orbitals)
-        and an integer num_populated, build a VQE that will apply X gates to the num_occupated
+        and an integer num_occupied, build a VQE that will apply X gates to the num_occupied
         qubits with the lowest bit significance.
         This is corresponding to providing a reference stat in the occupation number theory
         representation of quantum orbitals
@@ -35,14 +35,14 @@ class VQEBuilder:
         # note that qiskit is uses little endian notation, i.e. for example the lowest orbitals are
         # |000111> where the rightmost qubit would have index 0
         if num_qubits < num_occupied:
-            raise ValueError("Cannot have more occupated orbitals than qubits to represent them")
+            raise ValueError("Cannot have more occupied orbitals than qubits to represent them")
 
         ReferenceState(num_qubits, num_occupied)
-        return ReferenceState.reference_operator
+        return ReferenceState.get_ref_op()
 
     @staticmethod
-    def build_kUCC_ansatz_operator(self, num_qubits: int, eri1_ansatz_weights: NDArray, eri2_ansatz_weights: NDArray):
+    def build_kUCC_ansatz_operator(num_qubits: int, eri1_ansatz_weights: NDArray, eri2_ansatz_weights: NDArray):
         """Creates the Ansatz object for num_qubits, stores the operator"""
         ansatz = UCCAnsatz(num_qubits, eri1_ansatz_weights, eri2_ansatz_weights)
         ucc_ansatz = ansatz.get_ansatz()
-        return ucc_ansatz.exp_i()
+        return ucc_ansatz
--- src/VQE/VQEObject.py
+++ src/VQE/VQEObject.py
@@ -2,7 +2,12 @@
 operators needed and provides the methods necessary for measurements, gradients, expectation values
 and the likes."""
 
-from qiskit.opflow import OperatorBase
+from qiskit.circuit import ParameterVector, Parameter
+
+from qiskit.opflow import OperatorBase, PauliSumOp
+from qiskit.opflow.gradients import *
+from qiskit.opflow.expectations import *
+from qiskit.opflow.state_fns import CircuitStateFn
 
 from src.ansatz.RandomWeightGenerator import generate_random_2dim, generate_random_4dim
 from src.VQE.VQEBuilder import *
@@ -12,8 +17,11 @@ class VQEObject:
     num_qubits: int
     num_occupied: int
     reference_operator: OperatorBase = None
-    ansatz_operator: OperatorBase = None
-    hamiltonian_operator: OperatorBase = None
+    ansatz_operator: PauliSumOp = None
+    hamiltonian_operator: PauliSumOp = None
+
+    exp_ansatz_op: OperatorBase = None
+    exp_hamiltonian_op: OperatorBase = None
 
     # TODO: Wrap num_qubits, num_occupied, and all things specific to the molecule into a dedicated object
     def __init__(self, num_qubits: int, num_occupied: int):
@@ -27,6 +35,35 @@ class VQEObject:
         eri1_weights = generate_random_2dim(self.num_qubits)
         eri2_weights = generate_random_4dim(self.num_qubits)
         self.ansatz_operator = VQEBuilder.build_kUCC_ansatz_operator(self.num_qubits, eri1_weights, eri2_weights)
+        self.exp_ansatz_op = self.ansatz_operator.exp_i()
 
         # generate hamiltonian op
         self.hamiltonian_operator = VQEBuilder.build_hamiltonian_operator(self.num_qubits)
+        self.exp_hamiltonian_op = self.hamiltonian_operator.exp_i()
+
+    def measure(self):
+        """Method to perform a measurement onto the operators"""
+        pass
+
+    def full_op(self):
+        return self.exp_ansatz_op.adjoint().compose(self.exp_hamiltonian_op)
+
+    def expectation_value(self):
+        """Method to retrieve the expectation value """
+        # create |000...> state to measure
+        psi0 = CircuitStateFn(QuantumCircuit(self.num_qubits))
+        # do measurement <psi|U_ref^dag e^H U_ref|psi>
+        return psi0.adjoint().compose(self.exp_ansatz_op.adjoint()).compose(self.exp_hamiltonian_op) \
+            .compose(self.exp_ansatz_op).compose(psi0)
+
+    def gradient(self):
+        """Method to retrieve the first order gradient for the respective hamiltonian"""
+        # TODO: Change
+        # a = Parameter('a')
+        # theta_vec = [a, b, c]
+        gradient = Gradient().convert(self.full_op(), params=[])
+        return gradient
+
+    def hamiltonian_matrix(self):
+        """Method to create the matrix representation of the hamiltonian operator and return it"""
+        return self.hamiltonian_operator.to_matrix()
