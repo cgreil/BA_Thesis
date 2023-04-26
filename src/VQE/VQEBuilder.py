27b205b2f8baa0ec0f678b47509eca7df692a127
christoph <c.moser400@gmail.com>
2023-04-26 10:05:13 +0200

    Added VQE Calculation

--- src/VQE/VQEBuilder.py
+++ src/VQE/VQEBuilder.py
@@ -21,7 +21,7 @@ class VQEBuilder:
     def build_hamiltonian_operator(num_qubits, molecule: AbstractMolecule):
         """Creates the hamiltonian class for num_qubits, stores the operator"""
         hamiltonian = FermionicHamiltonian(num_qubits, molecule)
-        hamiltonian_operator = hamiltonian.get_hamiltonian()
+        hamiltonian_operator = hamiltonian.get_hamiltonian_op()
 
         return hamiltonian_operator
 
